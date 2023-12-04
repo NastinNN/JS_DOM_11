95793dfc6eab4dce9c37b588cd538ea4b47d5b43
NastinNN <nastya.mannova@gmail.com>
2023-12-04 20:06:25 +0300

    Отредактировано

---
+++ data/objects.js
@@ -0,0 +1,43 @@
+export let products = [
+      {
+            nameProduct: "Фитнес-браслет DEXP SB60",
+            imgSrc: "./image/DEXPSB60.webp",
+            corpus: "черный",
+            remen: "черный",
+            displaySize: "0,96\"",
+            displayType: "OLED",
+            bluetooth: true,
+            OS: {
+                  os1: "Android 4.2",
+                  os2: "iOS 8",
+            }
+      },
+
+      {
+            nameProduct: "Смарт-часы RunGo W2",
+            imgSrc: "./image/RunGoW2.webp",
+            corpus: "зеленый",
+            remen: "зеленый",
+            displaySize: "3\"",
+            displayType: "TFT (TN)",
+            bluetooth: true,
+            OS: {
+                  os1: "Android",
+                  os2: "iOS",
+            }
+      },
+
+      {
+            nameProduct: "Смарт-часы Welbe Prime",
+            imgSrc: "./image/WelbePrime.webp",
+            corpus: "черный",
+            remen: "розовый",
+            displaySize: "1.69\"",
+            displayType: "IPS",
+            bluetooth: true,
+            OS: {
+                  os1: "Android 5.0",
+                  os2: "iOS 9",
+            }
+      },
+]

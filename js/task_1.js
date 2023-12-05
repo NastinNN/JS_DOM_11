7ada48587dc35325bd90df8c935ad4397fc0eb7e
NastinNN <nastya.mannova@gmail.com>
2023-12-05 18:55:33 +0300

    Заработало с модулями

--- js/task_1.js
+++ js/task_1.js
@@ -1,46 +1,4 @@
-let products = [
-      {
-            nameProduct: "Фитнес-браслет DEXP SB60",
-            imgSrc: "./image/DEXPSB60.webp",
-            corpus: "черный",
-            remen: "черный",
-            displaySize: "0,96\"",
-            displayType: "OLED",
-            bluetooth: true,
-            OS: {
-                  os1: "Android 4.2",
-                  os2: "iOS 8",
-            }
-      },
-
-      {
-            nameProduct: "Смарт-часы RunGo W2",
-            imgSrc: "./image/RunGoW2.webp",
-            corpus: "зеленый",
-            remen: "зеленый",
-            displaySize: "3\"",
-            displayType: "TFT (TN)",
-            bluetooth: true,
-            OS: {
-                  os1: "Android",
-                  os2: "iOS",
-            }
-      },
-
-      {
-            nameProduct: "Смарт-часы Welbe Prime",
-            imgSrc: "./image/WelbePrime.webp",
-            corpus: "черный",
-            remen: "розовый",
-            displaySize: "1.69\"",
-            displayType: "IPS",
-            bluetooth: true,
-            OS: {
-                  os1: "Android 5.0",
-                  os2: "iOS 9",
-            }
-      },
-]
+import {products} from './objects.js';
 
 let cards=Array.from(document.querySelectorAll(".card"));
 cards.forEach(function(elem,index)

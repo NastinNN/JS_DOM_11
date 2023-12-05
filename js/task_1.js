f78191d64bc711b436d0d303dc228f8719946a3b
NastinNN <nastya.mannova@gmail.com>
2023-12-05 18:36:41 +0300

    Задача 1

--- js/task_1.js
+++ js/task_1.js
@@ -1,9 +1,51 @@
-import {products} from "./data/onjects.js";
+let products = [
+      {
+            nameProduct: "Фитнес-браслет DEXP SB60",
+            imgSrc: "./image/DEXPSB60.webp",
+            corpus: "черный",
+            remen: "черный",
+            displaySize: "0,96\"",
+            displayType: "OLED",
+            bluetooth: true,
+            OS: {
+                  os1: "Android 4.2",
+                  os2: "iOS 8",
+            }
+      },
+
+      {
+            nameProduct: "Смарт-часы RunGo W2",
+            imgSrc: "./image/RunGoW2.webp",
+            corpus: "зеленый",
+            remen: "зеленый",
+            displaySize: "3\"",
+            displayType: "TFT (TN)",
+            bluetooth: true,
+            OS: {
+                  os1: "Android",
+                  os2: "iOS",
+            }
+      },
+
+      {
+            nameProduct: "Смарт-часы Welbe Prime",
+            imgSrc: "./image/WelbePrime.webp",
+            corpus: "черный",
+            remen: "розовый",
+            displaySize: "1.69\"",
+            displayType: "IPS",
+            bluetooth: true,
+            OS: {
+                  os1: "Android 5.0",
+                  os2: "iOS 9",
+            }
+      },
+]
 
 let cards=Array.from(document.querySelectorAll(".card"));
 cards.forEach(function(elem,index)
 {
-      elem.querySelector("img").src=`img/${products[index].imgSrc}`;
+      elem.querySelector("img").src=`${products[index].imgSrc}`;
       elem.querySelector("h5").innerHTML=`<br>${products[index].nameProduct}`;
       let str=`<br>Корпус: ${products[index].corpus}
       <br>Ремешок: ${products[index].remen}

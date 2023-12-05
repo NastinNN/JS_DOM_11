e75c1e389b1c2b872ec09a36e04e8c1dda59ba39
NastinNN <nastya.mannova@gmail.com>
2023-12-05 19:01:34 +0300

    Исправление задачи 2

--- js/task_2.js
+++ js/task_2.js
@@ -3,11 +3,11 @@ let end=new Date(2024,0,1);
 function countdown(){
         let now=new Date();
         let days=Math.floor((end-now)/(1000*60*60*24));
-        let hours=24-now.getHours();;
-        let minutes=60-now.getMinutes();
-        let seconds=60-now.getSeconds();
+        let hours=Math.round((end-now)/(1000*60*60)%24);
+        let minutes=Math.round((end-now)/(1000*60)%60);
+        let seconds=Math.round((end-now)/(1000)%60);
         let str=`${days} дней ${hours} часов ${minutes} минут ${seconds} секунд`;
     document.querySelector(".timer").innerHTML=str;
 }
 
-setInterval("countdown()",0);
+setInterval("countdown()",1000);

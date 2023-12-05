ba39bfc76ede4d7eb76e82a778ba60a95d9a1319
NastinNN <nastya.mannova@gmail.com>
2023-12-05 18:31:50 +0300

    Задача 1

--- js/task_1.js
+++ js/task_1.js
@@ -1,2 +1,19 @@
-import {products} from "./js/onjects.js";
+import {products} from "./data/onjects.js";
 
+let cards=Array.from(document.querySelectorAll(".card"));
+cards.forEach(function(elem,index)
+{
+      elem.querySelector("img").src=`img/${products[index].imgSrc}`;
+      elem.querySelector("h5").innerHTML=`<br>${products[index].nameProduct}`;
+      let str=`<br>Корпус: ${products[index].corpus}
+      <br>Ремешок: ${products[index].remen}
+      <br>Размер дисплея: ${products[index].displaySize}
+      <br>Тип дисплея: ${products[index].displayType}`;
+      if (products[index].bluetooth)
+      str+=`<br>Блютуз: есть`;
+      else
+      str+=`<br>Блютуз: нет`;
+      str+=`<br>Операционная система: ${products[index].OS.os1}, ${products[index].OS.os2}`;
+      elem.querySelector(".card-text").innerHTML=str;
+}
+)
